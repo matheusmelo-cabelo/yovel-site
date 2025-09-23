a1722da5badc7313b58791f3380512fbebe92f82
matheusmelo-cabelo <backup.matheusmelo@gmail.com>
2025-09-23 00:44:41 -0300

    fix: Corrigir erro de build do Vercel para o hook useIntersection\n\nRenomeado src/useIntersection.js para src/useintersection.js e atualizado seu caminho de importação em src/App.jsx para resolver problemas de sensibilidade a maiúsculas e minúsculas encontrados durante os builds do Vercel.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useRef, useEffect } from 'react';
-import { useIntersection } from './useIntersection';
+import { useIntersection } from './useintersection';
 import './App.css';
 
 // --- Dicionário de Traduções ---
